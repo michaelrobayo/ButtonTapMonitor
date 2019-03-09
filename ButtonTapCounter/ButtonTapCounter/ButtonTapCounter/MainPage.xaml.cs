26f6e6fd03d09ff53527d98060cb657d971630f4
Michael Robayo <michael.robayo@gastag.co.uk>
2019-03-09 08:48:27 +0000

    Adding initial button setup
    I added in the default setup for this app which includes a label and button

---
+++ ButtonTapCounter/ButtonTapCounter/ButtonTapCounter/MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading;
+using Xamarin.Forms;
+
+namespace ButtonTapCounter
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private int _count = 0;
+
+        private void Button_Clicked(object sender, System.EventArgs e)
+        {
+            _count++;
+            ((Button)sender).Text = $"You clicked {_count} times.";
+        }
+    }
+}
